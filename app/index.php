1eb30677ac9a04531895e9b23cf6500597f53144
DESKTOP-3R075O2\User <kostiv8888@gmail.com>
2017-04-14 15:23:59 +0300

    commit

--- app/index.php
+++ app/index.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
 <html>
 
-<head>
+	<head>
 
-	<meta charset="utf-8">
+		<meta charset="utf-8">
 
-	<title>Title</title>
-	<meta name="description" content="">
+		<title>Title</title>
+		<meta name="description" content="">
 
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-	<meta property="og:image" content="path/to/image.jpg">
+		<meta property="og:image" content="path/to/image.jpg">
 
-	<!-- Load Styles -->
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<!-- Load Styles -->
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
 
-</head>
+	</head>
 
-<body>
+	<body>
 
-	<!-- Insert your code -->
+		<!-- Start coding -->
 
-	<!--[if lt IE 9]>
-	<script src="libs/html5shiv/es5-shim.min.js"></script>
-	<script src="libs/html5shiv/html5shiv.min.js"></script>
-	<script src="libs/html5shiv/html5shiv-printshiv.min.js"></script>
-	<script src="libs/respond/respond.min.js"></script>
-	<![endif]-->
+		<!--[if lt IE 9]>
+		<script src="libs/html5shiv/es5-shim.min.js"></script>
+		<script src="libs/html5shiv/html5shiv.min.js"></script>
+		<script src="libs/html5shiv/html5shiv-printshiv.min.js"></script>
+		<script src="libs/respond/respond.min.js"></script>
+		<![endif]-->
 
-	<!-- Load Scripts -->
-	<script src="js/common.js" type="text/javascript"></script>
+		<!-- Load Scripts -->
+		<script src="js/common.js" type="text/javascript"></script>
 
-</body>
+	</body>
 </html>
